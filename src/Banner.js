1eda25d92592e588ad5892fbb6fa0e675057ff6f
Chris Saffron <saffronkrzysztof@gmail.com>
2023-03-08 01:23:00 +0100

    initial upload

---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <header className='banner' style={{backgroundImage: `url(./banner.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center center'}}>
+aa
+    </header>
+  )
+}
+
+export default Banner
